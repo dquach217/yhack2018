faf65fbe2b89e5bb2dd7b975c8f3f7b4d38f2d3a
dquach217 <dq6180@rit.edu>
2018-12-01 06:40:35 -0500

    fix architecture of code, and remove unused code

---
+++ cheap-gas/components/gmapsDirections.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+import getDirections from 'react-native-google-maps-directions'
+ 
+export default class gmapsDirections extends Component {
+ 
+  handleGetDirections = () => {
+    const data = {
+       source: {
+        latitude: Number(sourceLatitude),
+        longitude: Number(sourceLongitude)
+      },
+      destination: {
+        latitude: Number(destinationLatitude),
+        longitude: Number(destinationLongitude)
+      },
+      params: [
+        {
+          key: "travelmode",
+          value: "driving"        // may be "walking", "bicycling" or "transit" as well
+        },
+        {
+          key: "dir_action",
+          value: "navigate"       // this instantly initializes navigation using the given travel mode 
+        }
+      ]
+    }
+ 
+    getDirections(data)
+  }
+ 
+  render() {
+    return (
+      <View style={{}}>
+        <Button onPress={this.handleGetDirections} title="Get Directions" />
+      </View>
+    );
+  }
+}
