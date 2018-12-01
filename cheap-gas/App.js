f52eb2b6f7a87c8411b85e1822ec474559e8aab5
David Martellock <dsm1252@rit.edu>
2018-12-01 08:38:15 -0500

    Pseudocode for project added to App.js
    Add clarity of some kind

---
+++ cheap-gas/App.js
@@ -0,0 +1,18 @@
+
+//IDEA: Make it easy to know how long it will take and where to go to charge your electric car
+
+//Pesudocode:
+
+//App main page:
+//  Display logo, battery level and number of miles left
+// IF number of miles left is below threshold value then:
+// Bring up alert form -> "your car battery is low, do you want me to find a charging station?"
+
+
+// If no > do nothing
+
+// If yes to ^ then:
+// ======= display list of charging stations WITH
+//    the actual time it will take you to charge to full
+
+//once one is selected: open a map to navigate there.
